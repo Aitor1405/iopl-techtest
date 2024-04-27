3f3f017daf2bddb714e60112d5d99bd621596d46
Aitor Cortés Sáez <acortes@okode.com>
2024-04-27 09:59:45 +0200

    feat: modify controller tests to avoid testing all layers and just mock the application one, since the others have been previously tested independently

--- src/test/java/com/iopl/techtest/infrastructure/rest/api/find_effective_product_price/FindEffectiveProductPriceControllerTests.java
+++ src/test/java/com/iopl/techtest/infrastructure/rest/api/find_effective_product_price/FindEffectiveProductPriceControllerTests.java
@@ -1,53 +1,61 @@
 package com.iopl.techtest.infrastructure.rest.api.find_effective_product_price;
 
-import com.iopl.techtest.pricing.infrastructure.persistence.jpa.JpaProductPriceRepository;
-import com.iopl.techtest.pricing.infrastructure.persistence.jpa.JpaProperties;
+import com.iopl.techtest.pricing.application.find_effective_product_price.FindEffectiveProductPriceUseCase;
+import com.iopl.techtest.pricing.domain.*;
 import com.iopl.techtest.pricing.infrastructure.rest.api.find_effective_product_price.EffectivePriceNotFoundError;
 import com.iopl.techtest.pricing.infrastructure.rest.api.find_effective_product_price.FindEffectiveProductPriceResponse;
 import com.iopl.techtest.pricing.infrastructure.rest.error.BadRequestApiError;
 import com.iopl.techtest.pricing.infrastructure.shared.model.Price;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
-import org.springframework.test.context.jdbc.Sql;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 
 @SpringBootTest(webEnvironment = RANDOM_PORT)
 public class FindEffectiveProductPriceControllerTests {
 
+    private static final ZoneOffset TEST_ZONE_OFFSET = ZoneOffset.UTC;
+
     @LocalServerPort
     private int port;
 
     @Autowired
-    private JpaProperties jpaProperties;
-
-    @Autowired
     private TestRestTemplate testRestTemplate;
 
-    @AfterEach
-    void clearDatabase(@Autowired JpaProductPriceRepository jpaProductPriceRepository) {
-        jpaProductPriceRepository.deleteAll();
-    }
+    @MockBean
+    private FindEffectiveProductPriceUseCase findEffectiveProductPriceUseCase;
+
 
     @Test
-    @Sql("test-data.sql")
-    public void effectiveProductPriceShouldReturnFirstRecord() {
+    public void effectiveProductPricePathVariables() {
+        var brandId = new BrandId(1);
+        var productId = new ProductId(35455);
+        var startAt = LocalDateTime.of(2020, 6, 14, 0, 0, 0).toInstant(TEST_ZONE_OFFSET);
+        var endAt = LocalDateTime.of(2020, 12, 31, 23, 59, 59).toInstant(TEST_ZONE_OFFSET);
+        var priceListId = new PriceListId(1);
+        var priority = new PricePriority(0);
+        var price = new com.iopl.techtest.pricing.domain.Price(new BigDecimal("35.50"), new Currency("EUR"));
         var datetime = LocalDateTime.of(2020, 6, 14, 10, 0, 0);
-        var effectiveInstant = datetime.toInstant(jpaProperties.getZoneOffset());
+        var effectiveInstant = datetime.toInstant(TEST_ZONE_OFFSET);
+        var expectedProductPrice = new ProductPrice(brandId, startAt, endAt, priceListId, productId, priority, price);
+        Mockito.when(findEffectiveProductPriceUseCase.findEffectiveProductPriceBy(brandId, productId, effectiveInstant)).thenReturn(expectedProductPrice);
+
         var expectedResponse = new FindEffectiveProductPriceResponse(
-                35455, 1, 1,
-                LocalDateTime.of(2020, 6, 14, 0, 0, 0).toInstant(jpaProperties.getZoneOffset()),
-                LocalDateTime.of(2020, 12, 31, 23, 59, 59).toInstant(jpaProperties.getZoneOffset()),
-                new Price(new BigDecimal("35.50"), "EUR")
+                productId.value(), brandId.value(), priceListId.value(),
+                startAt,
+                endAt,
+                new Price(price.amount(), price.currency().code())
         );
         var url = "http://localhost:" + port + "/api/brands/" + expectedResponse.brandId() + "/products/" + expectedResponse.productId() + "/prices?effectiveInstant=" + effectiveInstant;
         var response = testRestTemplate.getForObject(url, FindEffectiveProductPriceResponse.class);
@@ -55,79 +63,43 @@ public class FindEffectiveProductPriceControllerTests {
     }
 
     @Test
-    @Sql("test-data.sql")
-    public void effectiveProductPriceShouldReturnFirstRecord2() {
-        var datetime = LocalDateTime.of(2020, 6, 14, 21, 0, 0);
-        var effectiveInstant = datetime.toInstant(jpaProperties.getZoneOffset());
+    public void effectiveProductPriceSearchQuery() {
+        var brandId = new BrandId(1);
+        var productId = new ProductId(35455);
+        var startAt = LocalDateTime.of(2020, 6, 14, 0, 0, 0).toInstant(TEST_ZONE_OFFSET);
+        var endAt = LocalDateTime.of(2020, 12, 31, 23, 59, 59).toInstant(TEST_ZONE_OFFSET);
+        var priceListId = new PriceListId(1);
+        var priority = new PricePriority(0);
+        var price = new com.iopl.techtest.pricing.domain.Price(new BigDecimal("35.50"), new Currency("EUR"));
+        var datetime = LocalDateTime.of(2020, 6, 14, 10, 0, 0);
+        var effectiveInstant = datetime.toInstant(TEST_ZONE_OFFSET);
+        var expectedProductPrice = new ProductPrice(brandId, startAt, endAt, priceListId, productId, priority, price);
+        Mockito.when(findEffectiveProductPriceUseCase.findEffectiveProductPriceBy(brandId, productId, effectiveInstant)).thenReturn(expectedProductPrice);
+
         var expectedResponse = new FindEffectiveProductPriceResponse(
-                35455, 1, 1,
-                LocalDateTime.of(2020, 6, 14, 0, 0, 0).toInstant(jpaProperties.getZoneOffset()),
-                LocalDateTime.of(2020, 12, 31, 23, 59, 59).toInstant(jpaProperties.getZoneOffset()),
-                new Price(new BigDecimal("35.50"), "EUR")
+                productId.value(), brandId.value(), priceListId.value(),
+                startAt,
+                endAt,
+                new Price(price.amount(), price.currency().code())
         );
-        var url = "http://localhost:" + port + "/api/brands/" + expectedResponse.brandId() + "/products/" + expectedResponse.productId() + "/prices?effectiveInstant=" + effectiveInstant;
+        var url = "http://localhost:" + port + "/api/prices/search?brandId=" + expectedResponse.brandId() + "&productId=" + expectedResponse.productId() + "&effectiveInstant=" + effectiveInstant;
         var response = testRestTemplate.getForObject(url, FindEffectiveProductPriceResponse.class);
         Assertions.assertEquals(expectedResponse, response);
     }
 
     @Test
-    @Sql("test-data.sql")
-    public void effectiveProductPriceShouldReturnSecondRecord() {
-        var datetime = LocalDateTime.of(2020, 6, 14, 16, 0, 0);
-        var effectiveInstant = datetime.toInstant(jpaProperties.getZoneOffset());
-        var expectedResponse = new FindEffectiveProductPriceResponse(
-                35455, 1, 2,
-                LocalDateTime.of(2020, 6, 14, 15, 0, 0).toInstant(jpaProperties.getZoneOffset()),
-                LocalDateTime.of(2020, 6, 14, 18, 30, 0).toInstant(jpaProperties.getZoneOffset()),
-                new Price(new BigDecimal("25.45"), "EUR")
-        );
-        var url = "http://localhost:" + port + "/api/brands/" + expectedResponse.brandId() + "/products/" + expectedResponse.productId() + "/prices?effectiveInstant=" + effectiveInstant;
-        var response = testRestTemplate.getForObject(url, FindEffectiveProductPriceResponse.class);
-        Assertions.assertEquals(expectedResponse, response);
-    }
-
-    @Test
-    @Sql("test-data.sql")
-    public void effectiveProductPriceShouldReturnThirdRecord() {
-        var datetime = LocalDateTime.of(2020, 6, 15, 10, 0, 0);
-        var effectiveInstant = datetime.toInstant(jpaProperties.getZoneOffset());
-        var expectedResponse = new FindEffectiveProductPriceResponse(
-                35455, 1, 3,
-                LocalDateTime.of(2020, 6, 15, 0, 0, 0).toInstant(jpaProperties.getZoneOffset()),
-                LocalDateTime.of(2020, 6, 15, 11, 0, 0).toInstant(jpaProperties.getZoneOffset()),
-                new Price(new BigDecimal("30.50"), "EUR")
-        );
-        var url = "http://localhost:" + port + "/api/brands/" + expectedResponse.brandId() + "/products/" + expectedResponse.productId() + "/prices?effectiveInstant=" + effectiveInstant;
-        var response = testRestTemplate.getForObject(url, FindEffectiveProductPriceResponse.class);
-        Assertions.assertEquals(expectedResponse, response);
-    }
-
-    @Test
-    @Sql("test-data.sql")
-    public void effectiveProductPriceShouldReturnFourthRecord() {
-        var datetime = LocalDateTime.of(2020, 6, 16, 21, 0, 0);
-        var effectiveInstant = datetime.toInstant(jpaProperties.getZoneOffset());
-        var expectedResponse = new FindEffectiveProductPriceResponse(
-                35455, 1, 4,
-                LocalDateTime.of(2020, 6, 15, 16, 0, 0).toInstant(jpaProperties.getZoneOffset()),
-                LocalDateTime.of(2020, 12, 31, 23, 59, 59).toInstant(jpaProperties.getZoneOffset()),
-                new Price(new BigDecimal("38.95"), "EUR")
-        );
-        var url = "http://localhost:" + port + "/api/brands/" + expectedResponse.brandId() + "/products/" + expectedResponse.productId() + "/prices?effectiveInstant=" + effectiveInstant;
-        var response = testRestTemplate.getForObject(url, FindEffectiveProductPriceResponse.class);
-        Assertions.assertEquals(expectedResponse, response);
-    }
-
-    @Test
-    @Sql("test-data.sql")
     public void effectiveProductPriceNotFound() {
+        var brandId = new BrandId(1);
+        var productId = new ProductId(35455);
         var datetime = LocalDateTime.of(2024, 6, 16, 21, 0, 0);
-        var effectiveInstant = datetime.toInstant(jpaProperties.getZoneOffset());
-        var error = new EffectivePriceNotFoundError(1, 35455, effectiveInstant);
-        var url = "http://localhost:" + port + "/api/brands/" + error.getBrandId() + "/products/" + error.getProductId() + "/prices?effectiveInstant=" + effectiveInstant;
+        var effectiveInstant = datetime.toInstant(TEST_ZONE_OFFSET);
+        var ex = new FindEffectiveProductPriceUseCase.FindEffectivePriceUseCaseException.NotFound("No effective product price found.", brandId, productId, effectiveInstant);
+        var expectedError = new EffectivePriceNotFoundError(brandId.value(), productId.value(), effectiveInstant);
+        Mockito.when(findEffectiveProductPriceUseCase.findEffectiveProductPriceBy(brandId, productId, effectiveInstant)).thenThrow(ex);
+        var url = "http://localhost:" + port + "/api/brands/" + expectedError.getBrandId() + "/products/" + expectedError.getProductId() + "/prices?effectiveInstant=" + effectiveInstant;
         var responseEntity = testRestTemplate.getForEntity(url, EffectivePriceNotFoundError.class);
         Assertions.assertEquals(HttpStatus.NOT_FOUND, responseEntity.getStatusCode());
-        Assertions.assertEquals(error, responseEntity.getBody());
+        Assertions.assertEquals(expectedError, responseEntity.getBody());
     }
 
     @Test
